1b0f03ab96674b43232f649bc14250803de438d3
Leo Tschritter <leotschritter@gmail.com>
2025-10-28 14:22:33 +0100

    updated api files

--- src/api/apis/index.ts
+++ src/api/apis/index.ts
@@ -1,4 +1,7 @@
 /* tslint:disable */
 /* eslint-disable */
+export * from './CommentManagementApi';
 export * from './ItineraryManagementApi';
+export * from './LikeManagementApi';
+export * from './LocationManagementApi';
 export * from './UserManagementApi';
--- src/api/models/index.ts
+++ src/api/models/index.ts
@@ -1,6 +1,11 @@
 /* tslint:disable */
 /* eslint-disable */
+export * from './CommentDto';
+export * from './CommentRequest';
 export * from './ItineraryDto';
 export * from './ItinerarySearchDto';
 export * from './ItinerarySearchResponseDto';
+export * from './LikeDto';
+export * from './LikeResponseDto';
+export * from './LocationDto';
 export * from './UserDto';
