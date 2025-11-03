c55681e86b9961cb1f77e75a3fedaf13ed85fde9
Leo Tschritter <leotschritter@gmail.com>
2025-11-03 21:57:58 +0100

    refactor: remove unnecessary lifecycle ignore_changes for container image

--- terraform/main.tf
+++ terraform/main.tf
@@ -83,13 +83,6 @@ resource "google_cloud_run_v2_service" "main" {
       }
     }
   }
-
-
-  lifecycle {
-    ignore_changes = [
-      template[0].containers[0].image,
-    ]
-  }
 }
 
 # Data source for project information
