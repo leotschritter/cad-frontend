3b3d56c7f06fcc26468bdd024f53b6a8f4c13396
benediktscheffel <benedikt.scheffel@hotmail.de>
2025-11-04 13:43:51 +0100

    feat: add Firebase configuration as build arguments in Dockerfile and deployment script

--- gcloud_deployment.sh
+++ gcloud_deployment.sh
@@ -1,5 +1,9 @@
 # Build with API URL as build argument
-docker build --build-arg VITE_API_BASE_URL="$API_URL" -t de.htwg-konstanz.in/travel-frontend:"$VERSION" .
+docker build --build-arg VITE_API_BASE_URL="$API_URL" \
+    --build-arg VITE_FIREBASE_API_KEY="$VITE_FIREBASE_API_KEY" \
+    --build-arg VITE_FIREBASE_AUTH_DOMAIN="$VITE_FIREBASE_AUTH_DOMAIN" \
+    --build-arg VITE_FIREBASE_PROJECT_ID="$VITE_FIREBASE_PROJECT_ID" \
+    -t de.htwg-konstanz.in/travel-frontend:"$VERSION" .
 
 docker tag de.htwg-konstanz.in/travel-frontend:"$VERSION" europe-west1-docker.pkg.dev/"$PROJECT_ID"/docker-repo/travel-frontend:"$VERSION"
 docker push europe-west1-docker.pkg.dev/$PROJECT_ID/docker-repo/travel-frontend:"$VERSION"
